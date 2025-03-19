7424d53fa1e49b00cc5d9fb61a6997a87d717884
lionc <shizijian2010@yahoo.com.hk>
2025-03-19 08:48:14 +0800

    update contract

--- network_config.py
+++ network_config.py
@@ -10,11 +10,11 @@ networks = {
     'Base': {
         'rpc_url': 'https://sepolia.base.org',
         'chain_id': 84532,
-        'contract_address': '0x9D5cfa02e19dd0c3B05eCd1a3d9cf537FEE847d0'
+        'contract_address': '0xCEE0372632a37Ba4d0499D1E2116eCff3A17d3C3'
     },
     'OP Sepolia': {
         'rpc_url': 'https://endpoints.omniatech.io/v1/op/sepolia/public',
         'chain_id': 11155420,
-        'contract_address': '0xACa2218Fa847aECfe174fD1da80a90B7B3Ce4E17'
+        'contract_address': '0xb6Def636914Ae60173d9007E732684a9eEDEF26E'
     },
 }
