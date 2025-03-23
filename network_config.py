889c873f050570e4da88592e9c3313f142574eee
lionc <shizijian2010@yahoo.com.hk>
2025-03-23 10:20:07 +0800

    update

--- network_config.py
+++ network_config.py
@@ -8,12 +8,12 @@ load_dotenv()
 
 networks = {
     'Base': {
-        'rpc_url': 'https://base-sepolia.g.alchemy.com/v2/skX10ZnJcmh7VX25ejnqJw9ZRJekUIl0',
+        'rpc_url': 'https://base-sepolia.g.alchemy.com/v2/F4y7TAY7mwe1QnzySJKqESEQ6dzjQjZZ',
         'chain_id': 84532,
         'contract_address': '0xCEE0372632a37Ba4d0499D1E2116eCff3A17d3C3'
     },
     'OP Sepolia': {
-        'rpc_url': 'https://opt-sepolia.g.alchemy.com/v2/skX10ZnJcmh7VX25ejnqJw9ZRJekUIl0',
+        'rpc_url': 'https://opt-sepolia.g.alchemy.com/v2/F4y7TAY7mwe1QnzySJKqESEQ6dzjQjZZ',
         'chain_id': 11155420,
         'contract_address': '0xb6Def636914Ae60173d9007E732684a9eEDEF26E'
     },
