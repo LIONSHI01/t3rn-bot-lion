3337ab6cfbcd33f3ef65598b2f2de327f8f3fdd4
lionc <shizijian2010@yahoo.com.hk>
2025-03-24 22:15:37 +0800

    update wait time

--- privkey_update_env.sh
+++ privkey_update_env.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+read -p "Private Key:" PRIVATE_KEY
 read -p "Number of ETH to Bridge:" BRIDGE_AMOUNT
 read -p "Data from Base to OP:" DATA_BRIDGE_BASE_TO_OP
 read -p "Data from OP to Base:" DATA_BRIDGE_OP_TO_BASE
 
+sed -i "s/PRIVATE_KEY=.*/PRIVATE_KEY=$PRIVATE_KEY/" .env
 sed -i "s/BRIDGE_AMOUNT=.*/BRIDGE_AMOUNT=$BRIDGE_AMOUNT/" .env
 sed -i "s/DATA_BRIDGE_BASE_TO_OP=.*/DATA_BRIDGE_BASE_TO_OP=$DATA_BRIDGE_BASE_TO_OP/" .env
 sed -i "s/DATA_BRIDGE_OP_TO_BASE=.*/DATA_BRIDGE_OP_TO_BASE=$DATA_BRIDGE_OP_TO_BASE/" .env
